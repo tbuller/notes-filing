ea69111aa9b9b6be2a31804844b6784d139545dc
Tim Buller <111061411+tbuller@users.noreply.github.com>
2023-01-29 15:34:56 +0100

    port set up and new folders

---
+++ userStory.js
@@ -0,0 +1,9 @@
+// as a user I want to be able to create an account and log in
+
+// as a user, I want to be able to create a new note
+
+// as a user, I want to be able to create my own folders, and other folders within that
+
+// as a user, I want to be able to delete a note when I no longer need it
+
+// as a user, I want to be able to update the contents of a note
