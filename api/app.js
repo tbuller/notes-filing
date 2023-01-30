601db5cba8a4ec371a63bb30183e683e15642d79
Tim Buller <111061411+tbuller@users.noreply.github.com>
2023-01-30 12:16:26 +0100

    Home page rendered

--- api/app.js
+++ api/app.js
@@ -5,3 +5,19 @@ const logger = require("morgan");
 const JWT = require("jsonwebtoken");
 const fileUpload = require("express-fileupload");
 const TokenGenerator = require("./models/token_generator");
+
+const app = express()
+
+const logRequest = (req, res, next) => {
+  console.log(`Received ${req.method} from ${req.url}`)
+  next()
+}
+
+app.use('/api/notes-filing', logRequest)
+
+app.get('/', (req, res) => {
+  res.send('It works')
+  console.log(res)
+})
+
+module.exports = app;
