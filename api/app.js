5de3ce12be4334bcce590e03936cab3380a392b7
Tim Buller <111061411+tbuller@users.noreply.github.com>
2023-01-30 10:49:00 +0100

    more project set up

--- api/app.js
+++ api/app.js
@@ -1,1 +1,7 @@
-const express = require ('express');
+const createError = require("http-errors");
+const express = require("express");
+const path = require("path");
+const logger = require("morgan");
+const JWT = require("jsonwebtoken");
+const fileUpload = require("express-fileupload");
+const TokenGenerator = require("./models/token_generator");
