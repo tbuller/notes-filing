8d2117d05151b5f52a287efbdcbb6d1c6562d388
Tim Buller <111061411+tbuller@users.noreply.github.com>
2023-02-09 11:46:17 +0100

    context hook set up

--- frontend/src/context/NotesContext.js
+++ frontend/src/context/NotesContext.js
@@ -2,15 +2,28 @@ import { createContext, useReducer } from 'react';
 
 export const NotesContext = createContext()
 
+export const notesReducer = (state, action) => {
+  switch (action.type) {
+    case "SET_NOTES":
+      return {
+        notes: action.payload
+      }
+    case "CREATE_NOTE":
+      return {
+        notes: [action.payload, ...state.notes]
+      } 
+    default:
+      return state
+  }
+}
+
 export const NotesContextProvider = ({ children }) => {
   const [state, dispatch] = useReducer(notesReducer, {
     notes: null
   })
 
-  dispatch({ type: "SET_NOTES", payload: [{}, {}] })
-
   return (
-    <NotesContextProvider value={}>
+    <NotesContextProvider value={{state, dispatch}}>
       { children }
     </NotesContextProvider>
   )
